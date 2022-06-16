185e9ae28673ce91271e06d00de0348924d15f8c
apenavargas <apenavargas@ethz.ch>
2022-06-16 16:55:46 +0200

    - fixed script.js login function

--- src/main/java/ch/bbw/marvel/Frontend/FrontendController.java
+++ src/main/java/ch/bbw/marvel/Frontend/FrontendController.java
@@ -1,6 +1,7 @@
 package ch.bbw.marvel.Frontend;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.CookieValue;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
