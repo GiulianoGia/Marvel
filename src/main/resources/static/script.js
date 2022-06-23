0ba664ce38457068a138c45b0b70c457bbc85743
apenavargas <apenavargas@ethz.ch>
2022-06-23 21:57:52 +0200

    feat(Login): javaScript for form

--- src/main/resources/static/script.js
+++ src/main/resources/static/script.js
@@ -1,21 +1,20 @@
-function login(button) {
-    $(button).find("img").removeAttr("hidden");
-    $("#login_button").attr("disabled", true);
-    $("input").attr("disabled", true);
-
-    $.get("/user/login?email=" + $("#email").val() + "&password=" + $("#password").val(),
-        function(response) {
-            console.log(response);
-        if (response) {
-            $.cookie("current_user", response.userID + "." + response.sign, {expires: 1 / 24} );
-            window.location = "/";
-            $(button).find("img").attr("hidden", true);
-            $("#login_button").removeAttr("disabled");
-            $("input").removeAttr("disabled");
+$(document).ready(function() {
+    $("input").on("change", function () {
+        let isFilled = true;
+        $("input").each(function(index, element) {
+            if($(element).val() == null) {
+                isFilled = false;
+                return false;
+            }
+        });
+        if(isFilled) {
+            $("button[type=submit]").eq(0).removeAttr("disabled");
         }
-    });
-}
+    })
+});
 
-function register(element) {
-    $.ajax()
+// ---------------------------------------------------------------------------------------------------------------------
+// FORM FUNCTIONS
+function disable_input() {
+    $("form").children().attr("disabled", true);
 }
