cbaa2b2866dd05241c2c4d9990498a85a7f0d9cc
Giuliano Gianola <giuliano.gianola@gmx.ch>
2022-06-23 17:13:06 +0200

    feat(controller): you can only create one film with the same name

--- src/main/java/ch/bbw/marvel/backend/repository/FilmRepository.java
+++ src/main/java/ch/bbw/marvel/backend/repository/FilmRepository.java
@@ -16,7 +16,7 @@ public interface FilmRepository extends JpaRepository<Film, Integer> {
     List<Film> getAllFilm();
 
     @Query(value = "insert into film (filmid, cost, name, rating, image) VALUES (:id, :cost, :name, :rating, :image)", nativeQuery = true)
-    Film createFilm(@Param("id") Integer id, @Param("cost") Integer cost, @Param("name") String name, @Param("rating") Double rating);
+    Film createFilm(@Param("id") Integer id, @Param("cost") Integer cost, @Param("name") String name, @Param("rating") Double rating, @Param("image") String image);
 
     @Query(value="select * from film where name like :name%", nativeQuery = true)
     List<Film> getFilmByName(@Param("name") String name);
