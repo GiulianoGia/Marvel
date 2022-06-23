44c297d0d54b07493ce6bc38803f046961fb8702
apenavargas <apenavargas@ethz.ch>
2022-06-23 21:16:42 +0200

    feat(Login): made routes for login

--- src/main/java/ch/bbw/marvel/frontend/FrontendController.java
+++ src/main/java/ch/bbw/marvel/frontend/FrontendController.java
@@ -1,10 +1,22 @@
 package ch.bbw.marvel.frontend;
 
+import ch.bbw.marvel.frontend.models.User;
+import ch.bbw.marvel.frontend.service.LoginService;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.CookieValue;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+
+import javax.servlet.http.Cookie;
+import javax.servlet.http.HttpServletResponse;
 
 @Controller
 public class FrontendController {
+
+    private LoginService loginService = new LoginService();
+
 
     @GetMapping("/")
     public String index() {
@@ -13,11 +25,26 @@ public class FrontendController {
     }
 
     @GetMapping("/login")
-    public String login() {
+    public String login(@CookieValue(name="currentUser", defaultValue="undefined.undefined") String cookieValue, Model model) {
 
-        return "login";
+        String result = "login";
+        if(loginService.hasUser(cookieValue)) {
+            result = "redirect:/";
+        }
+        model.addAttribute("user", new User());
+        return result;
     }
 
-    p
+    @PostMapping("/login")
+    public String postLogin(@ModelAttribute User user, HttpServletResponse response) {
 
+        Cookie cookie = loginService.login(user);
+        String result = "login";
+        if(cookie != null) {
+            result = "redirect:/";
+            response.addCookie(cookie);
+        }
+
+        return result;
+    }
 }
