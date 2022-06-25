ac0b661de2b9aca4725881f6a9a17d118768656e
apenavargas <apenavargas@ethz.ch>
2022-06-25 19:01:42 +0200

    file(Config.java): added

---
+++ src/main/java/ch/bbw/marvel/frontend/Config.java
@@ -0,0 +1,7 @@
+package ch.bbw.marvel.frontend;
+
+public class Config {
+
+    public static final String SECRET_KEY = "_hogwarts";
+    public static final String API_HOST = "http://localhost:8080";
+}
