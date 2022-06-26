082e2a17c4acebebac6dd04f70fed3188031eac7
apenavargas <apenavargas@ethz.ch>
2022-06-26 18:34:50 +0200

    added new Marvel.sql file

--- src/main/java/ch/bbw/marvel/frontend/service/FrontendActorService.java
+++ src/main/java/ch/bbw/marvel/frontend/service/FrontendActorService.java
@@ -3,15 +3,14 @@ package ch.bbw.marvel.frontend.service;
 import ch.bbw.marvel.frontend.Config;
 import ch.bbw.marvel.frontend.models.Actor;
 import ch.bbw.marvel.frontend.models.Film;
+
 import org.springframework.core.ParameterizedTypeReference;
 import org.springframework.http.HttpMethod;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
 
-import java.util.ArrayList;
 import java.util.List;
-import java.util.Locale;
 
 @Service
 public class FrontendActorService {
