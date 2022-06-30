5a80f826c16cb29a9ebee674c258b141bf9e5356
Giuliano Gianola <giuliano.gianola@gmx.ch>
2022-06-30 09:06:35 +0200

    feat(Film_actor): new film request

--- src/main/java/ch/bbw/marvel/backend/controller/FilmActorController.java
+++ src/main/java/ch/bbw/marvel/backend/controller/FilmActorController.java
@@ -24,6 +24,12 @@ public class FilmActorController {
         return filmActorService.getAllFilmActor();
     }
 
+    // get film actors by film id
+    @GetMapping("/filmActors/filmID")
+    public List<FilmActor> getFilmActorsByFilmID(@RequestParam("filmID") int filmID) {
+        return filmActorService.getFilmActorsByFilmID(filmID);
+    }
+
     @PostMapping(value="/filmActor/create", params = {"filmID", "actorID"})
     public FilmActor createFilmActor(@RequestParam("filmID") int filmID, @RequestParam("actorID") int actorID) {
         logger.info("filmID: " + filmID + " actorID: " + actorID);
